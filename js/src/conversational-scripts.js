41709faabe121b2474dc7b64ea153a85b78af0f5
childressdev <jcampbell@childressagency.com>
2019-03-29 17:28:42 -0400

    styled through growth

--- js/src/conversational-scripts.js
+++ js/src/conversational-scripts.js
@@ -3,4 +3,35 @@ jQuery(document).ready(function($){
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, 'slow');
   });
+
+  // animation check
+  var $animationElements = $('.animation-trigger');
+  var $window = $(window);
+
+  // disable on small devices
+  var isMobile = window.matchMedia('(max-width:768px)');
+  if(isMobile.matches){
+    $animationElements.removeClass('animation-trigger');
+  }
+
+  function checkIfInView(){
+    var windowHeight = $window.height();
+    var windowTopPosition = $window.scrollTop();
+    var windowBottomPosition = (windowTopPosition + windowHeight);
+
+    $.each($animationElements, function(){
+      var $element = $(this);
+      var elementHeight = $element.outerHeight();
+      var elementTopPosition = $element.offset().top;
+      var elementHeightOffset = elementHeight * (75 / 100);
+      var elementBottomPosition = (elementTopPosition + elementHeight);
+
+      if((elementBottomPosition >= windowTopPosition) && 
+        (elementTopPosition <= windowBottomPosition - elementHeightOffset)){
+            $element.addClass('animation-go');
+      }
+    });
+  }
+
+  $window.on('scroll', checkIfInView);
 });
