95b8633e285b1c833df6db69e6951572ed9eac6e
childressdev <jcampbell@childressagency.com>
2019-04-09 18:28:22 -0400

    added

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+  <?php $page_id = get_the_ID(); ?>
+  <section id="hp-intro">
+    <div class="container-fluid">
+      <article class="standard-page">
+        <header class="page-header">
+          <?php echo wp_kses_post(get_post_meta($page_id, 'home_page_intro', true)); ?>
+        </header>
+      </article>
+    </div>
+  </section>
+
+  <?php get_template_part('partials/home-section', 'features'); ?>
+
+  <?php get_template_part('partials/home-section', 'growth'); ?>
+
+<?php get_footer();
